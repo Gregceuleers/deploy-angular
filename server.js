306f567a43671e24f9499f5c83c04eb89952d1e5
Ceuleers Grégory <gregory.ceuleers@bstorm.be>
2021-03-30 16:26:11 +0200

    config heroku server modif

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/deployflowerpower'));
+app.use(express.static(__dirname + '/dist/DeployAngular'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/deployflowerpower/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/DeployAngular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
